b8c1a599496c6166b699bfde6421141dd8159c98
Piotr Michalczuk <piotrekmichal91@gmail.com>
2017-03-06 11:33:12 +0100

    added minimized css file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
+var changed = require('gulp-changed');
 
 gulp.task('serve', ['sass'], function() {
 	gulp.watch('src/scss/**/*.scss', ['sass']);
@@ -17,5 +20,18 @@ gulp.task('sass', function() {
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('src/css'));
 });
+
+gulp.task('css', function() {
+	return gulp.src('src/css/**/*.css')
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('public/css'));
+})
+
+gulp.task('img', function() {
+	return gulp.src('src/img/**/*.{jpg,jpeg,png,gif}')
+		.pipe(changed('public/img'))
+		.pipe(imagemin())
+		.pipe(gulp.dest('public/img'));
+})
 
 gulp.task('default', ['serve']);
